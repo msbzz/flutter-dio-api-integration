dd2a9cefa31a4bc99226b781126fe3b136b5a086
Marco <mbarozzi@gmail.com>
2024-08-13 20:25:05 -0300

    preparando o conteudo do log e salvando no realtime database

--- flutter_listin/lib/_core/services/dio_interceptor.dart
+++ flutter_listin/lib/_core/services/dio_interceptor.dart
@@ -1,11 +1,33 @@
+import 'dart:convert';
+
 import 'package:dio/dio.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:logger/logger.dart';
 
 class DioInterceptor extends Interceptor {
-
+  Logger _logger =
+      Logger(printer: PrettyPrinter(methodCount: 0, printEmojis: false));
 
   @override
   void onRequest(RequestOptions options, RequestInterceptorHandler handler) {
-    print("${DateTime.now()} | Uma requisição foi feita!");
+    //print("${DateTime.now()} | Uma requisição foi feita!");
+    String log = "";
+    log += "REQUISIÇÃO\n";
+    log += "Timestamp: ${DateTime.now()}\n";
+    log += "Método: ${options.method}";
+    log += "URL: ${options.uri}\n";
+    log +=
+        "Cabeçalho: ${JsonEncoder.withIndent("  ").convert(options.headers)}\n";
+
+    if (options.data != null) {
+      log +=
+          "Corpo: ${JsonEncoder.withIndent('  ').convert(json.decode(options.data))}\n";
+    }
+    _logger.w(log);
+
+    Dio().post('${dotenv.env["FIREBASE_URL"]!}/logs.json',
+        data: {"request": log});
+
     super.onRequest(options, handler);
   }
 
@@ -20,4 +42,11 @@ class DioInterceptor extends Interceptor {
     print("${DateTime.now()} | Um erro aconteceu!");
     super.onError(err, handler);
   }
+
+  // final Logger _logger = Logger(
+  //   printer: PrettyPrinter(
+  //       methodCount: 0,
+  //       printEmojis: false,
+  //   ),
+  // )
 }
